367921438d0fb5212247fe3cafde08b3f05c5370
AsimoLoveGym <xiazhuangz@gmail.com>
2016-11-07 11:16:55 -0600

    [CHORE]Add necessary libraries and related files

---
+++ js/source/main.js
@@ -0,0 +1,20 @@
+class DisplayContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+
+  }
+
+  render() {
+    // const placeHolder = ''
+
+
+    return(
+    );
+  }
+}
+
+ReactDOM.render(
+  <DisplayContainer />,
+  document.getElementById('display-container')
+);
