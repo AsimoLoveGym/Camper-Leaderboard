56394d2fb0ba69f38c1ca0bc7ea52e46f689d292
AsimoLoveGym <xiazhuangz@gmail.com>
2016-11-09 22:46:49 -0600

    [FEAT] Add static ReactJS page, add some style. [TODO] More JS work for sorting

--- js/build/main.js
+++ js/build/main.js
@@ -1,33 +1,136 @@
 "use strict";
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function ObjectRow(props) {
+  // const tableContents = props.tableContents;
+  var userURL = "https://www.freecodecamp.com/" + props.item.username;
+
+  return React.createElement(
+    "tr",
+    null,
+    React.createElement(
+      "td",
+      null,
+      props.index + 1
+    ),
+    React.createElement(
+      "td",
+      null,
+      " ",
+      React.createElement(
+        "a",
+        { href: userURL, target: "_blank" },
+        React.createElement("img", { src: props.item.img, className: "user-img" }),
+        props.item.username
+      )
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.item.recent
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.item.alltime
+    )
+  );
+}
+
+function App(props) {
+  var tableData = props.tableData;
+  // const tableItems = tableData.map((item,index,array)=> {
+  //   // console.log(item);
+  //   <tr key={index}>
+  //     <td>{index}</td>
+  //     <td>{item.username}<img src={item.img} /></td>
+  //     <td>{item.recent}</td>
+  //     <td>{item.alltime}</td>
+  //   </tr>
+  // })
+  // console.log(tableItems);
+  console.log(tableData);
+  return React.createElement(
+    "table",
+    { className: "table table-striped table-bordered table-hover" },
+    React.createElement(
+      "caption",
+      null,
+      "Recent for points in past 30 days"
+    ),
+    React.createElement(
+      "colgroup",
+      null,
+      React.createElement("col", { className: "#" }),
+      React.createElement("col", { className: "camper" }),
+      React.createElement("col", { className: "recent" }),
+      React.createElement("col", { className: "total" })
+    ),
+    React.createElement(
+      "thead",
+      null,
+      React.createElement(
+        "tr",
+        null,
+        React.createElement(
+          "th",
+          { scope: "col" },
+          "#"
+        ),
+        React.createElement(
+          "th",
+          { scope: "col" },
+          "Camper"
+        ),
+        React.createElement(
+          "th",
+          { scope: "col", id: "recent-points" },
+          "Recent   ",
+          React.createElement(
+            "span",
+            { className: "badge badge-on" },
+            React.createElement("i", { className: "fa fa-sort-desc", "aria-hidden": "true" })
+          )
+        ),
+        React.createElement(
+          "th",
+          { scope: "col", id: "total-points" },
+          "Total   ",
+          React.createElement(
+            "span",
+            { className: "badge" },
+            React.createElement("i", { className: "fa fa-sort", "aria-hidden": "true" })
+          )
+        )
+      )
+    ),
+    React.createElement(
+      "tbody",
+      null,
+      [].concat(_toConsumableArray(tableData)).map(function (item, index, array) {
+        return React.createElement(ObjectRow, { key: index + 1, index: index, item: item });
+      })
+    )
+  );
+}
+
+$(document).ready(function () {
+  var queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+  $.get(queryString, successFunction);
+});
+
 $("#recent-points").click(function () {
   var queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
   $.get(queryString, successFunction);
 });
 
-// https://fcctop100.herokuapp.com/api/fccusers/top/alltime
+$("#total-points").click(function () {
+  var queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
+  $.get(queryString, successFunction);
+});
 
 function successFunction(data, status) {
-  console.log(data);
+  // console.log(data);
+  ReactDOM.render(React.createElement(App, { tableData: data }), document.getElementById('table-container'));
 }
-
-// class DisplayContainer extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//
-//   }
-//
-//   render() {
-//     // const placeHolder = ''
-//
-//
-//     return(
-//     );
-//   }
-// }
-//
-// ReactDOM.render(
-//   <DisplayContainer />,
-//   document.getElementById('leaderboard-tbody')
-// );
--- js/source/main.js
+++ js/source/main.js
@@ -1,31 +1,82 @@
-$("#recent-points").click(function(){
-  var queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+"use strict";
+
+function ObjectRow(props) {
+  // const tableContents = props.tableContents;
+  const userURL = "https://www.freecodecamp.com/"+props.item.username;
+
+  return(
+    <tr>
+      <td>{props.index + 1}</td>
+      <td> <a href={userURL} target="_blank"><img src={props.item.img} className="user-img" />{props.item.username}</a></td>
+      <td>{props.item.recent}</td>
+      <td>{props.item.alltime}</td>
+    </tr>
+  );
+}
+
+function App(props) {
+  const tableData = props.tableData;
+  // const tableItems = tableData.map((item,index,array)=> {
+  //   // console.log(item);
+  //   <tr key={index}>
+  //     <td>{index}</td>
+  //     <td>{item.username}<img src={item.img} /></td>
+  //     <td>{item.recent}</td>
+  //     <td>{item.alltime}</td>
+  //   </tr>
+  // })
+  // console.log(tableItems);
+  console.log(tableData);
+  return(
+    <table className="table table-striped table-bordered table-hover">
+      <caption>
+        Recent for points in past 30 days
+      </caption>
+      <colgroup>
+        <col className="#" />
+        <col className="camper" />
+        <col className="recent" />
+        <col className="total" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Camper</th>
+          <th scope="col" id="recent-points">Recent   <span className="badge badge-on"><i className="fa fa-sort-desc" aria-hidden="true"></i></span></th>
+          <th scope="col" id="total-points">Total   <span className="badge"><i className="fa fa-sort" aria-hidden="true"></i></span></th>
+        </tr>
+      </thead>
+      <tbody>
+        {[...tableData].map((item,index,array)=>
+          <ObjectRow key={index+1} index={index} item={item} />
+        )}
+      </tbody>
+     </table>
+  );
+}
+
+
+
+$(document).ready(function() {
+  const queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
   $.get(queryString, successFunction);
 });
 
-// https://fcctop100.herokuapp.com/api/fccusers/top/alltime
+$("#recent-points").click(function () {
+  const queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+  $.get(queryString, successFunction);
+});
+
+$("#total-points").click(function () {
+  const queryString = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
+  $.get(queryString, successFunction);
+});
+
 
 function successFunction(data, status) {
-  console.log(data);
+  // console.log(data);
+  ReactDOM.render(
+    <App tableData={data}/>,
+    document.getElementById('table-container')
+  );
 }
-
-// class DisplayContainer extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//
-//   }
-//
-//   render() {
-//     // const placeHolder = ''
-//
-//
-//     return(
-//     );
-//   }
-// }
-//
-// ReactDOM.render(
-//   <DisplayContainer />,
-//   document.getElementById('leaderboard-tbody')
-// );
